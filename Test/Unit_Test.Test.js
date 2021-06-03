8b83df27b9ba61447609754d5a889708639a1e1b
Guy Shimony <shimonyguy@gmail.com>
2021-06-03 10:59:39 +0300

    login tests

--- Test/Unit_Test.Test.js
+++ Test/Unit_Test.Test.js
@@ -1,25 +1,50 @@
 var axios = require("axios");
 const assert = require("assert");
+const { Console } = require("console");
 
-const api_domain = "https://localhost:3000";
+const api_domain = "http://localhost:3000";
 
 
-async function getLoginStatus(username, password){
-    console.log("HELLO")
-    const auth = await axios.post(`${api_domain}/login`, {
-        data: {
-          username: username,
-          password: password
-        },
-      })
-    console.log("HELLO")
-    return auth.status
-}
 describe('Login Test', () => {
- it('correct login should return status code 200', async () => {
-        console.log("dsfsdfd")
-        let test_result = await getLoginStatus("Johnc","1234John")
-        console.log(test_result)
-        assert.equal(test_result, 200);
+    it('correct login should return status code 200', async () => {
+        let res = await axios.post(`${api_domain}/login`, {
+                username: "Johnc",
+                password: "1234John"
+            })
+            assert.equal(res.status, 200);
+        });
+    it('incorrect login should return status code 401', async (done) => {
+        try{
+            let res2 = await axios.post(`${api_domain}/login`, {
+                    username: "JohnCsf",
+                    password: "1234John"
+                })
+            console.log(res2.status)
+            assert.equal(res2.status, 401);
+            done()
+        }
+        catch(err){
+            assert.equal(err, 401);}
+            done(err)
+        }
+
+        );
+
+    it('incorrect login should return status code 401', async () => {
+        let res3 = await axios.post(`${api_domain}/login`, {
+                username: "Johnc",
+                password: "124John"
+            })
+        console.log(res3.status)
+        assert.equal(res3.status, 401);
     });
+    it('incorrect login should return status code 401', async () => {
+        let res4 = await axios.post(`${api_domain}/login`, {
+                username: "",
+                password: ""
+            })
+        assert.equal(res4.status, 401);
+    });
+
+
 });
