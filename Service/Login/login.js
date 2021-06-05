7f265d3a1a31f8647ecee5201b5a1e6e5b7d06eb
edenyav <edenyav@post.bgu.ac.il>
2021-06-05 12:12:04 +0300

    Service Layer init

---
+++ Service/Login/login.js
@@ -0,0 +1,8 @@
+var axios = require("axios");
+
+/*
+This module represent the service component for the login request send by the user.
+For tests purposes we will use a fake user that already exists in the DB and wants to login
+*/
+
+axios.post
