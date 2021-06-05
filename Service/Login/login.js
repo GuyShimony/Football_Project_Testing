d9fc2b3f4e55dfba80c809dd1a9a3a0882cdf027
edenyav <edenyav@post.bgu.ac.il>
2021-06-05 12:37:45 +0300

    Upload Service

--- Service/Login/login.js
+++ Service/Login/login.js
@@ -1,8 +1,23 @@
 var axios = require("axios");
-
+const api_domain = "http://localhost:3000";
+const cookie = ""
 /*
 This module represent the service component for the login request send by the user.
 For tests purposes we will use a fake user that already exists in the DB and wants to login
 */
 
-axios.post
+
+function login()
+{
+    return axios.post(`${api_domain}/login`, {username: "Johnc", password: "1234John"})
+
+}
+
+login().then(result => {
+    console.log(result.status, result.data)
+    
+  }).catch(error => {
+    console.log(error)
+  })
+
+  
