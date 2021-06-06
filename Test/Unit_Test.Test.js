47f521ab6aff1ae4224215ae02e00791c5b50259
edenyav <edenyav@post.bgu.ac.il>
2021-06-06 23:07:25 +0300

    refacture unit testing

--- Test/Unit_Test.Test.js
+++ Test/Unit_Test.Test.js
@@ -12,7 +12,7 @@ function importTest(name, path) {
 /*
 The main file that will run each test.
 */
-describe("top", function () {
+describe("Starting to run tests", function () {
 
   const common = require("./common.js");
 
@@ -20,9 +20,9 @@ describe("top", function () {
     console.log( "Creating a fake user name for the api tests in the DB");
     await common.createFakeUser();
   });
-  // importTest("Game scheduling", "./Game_Schedule/Games_Sched_Unit_Testing.js");
+  importTest("Game scheduling", "./Game_Schedule/Games_Sched_Unit_Testing.js");
   //importTest("Login", "./API/LoginAPI_Unit_Testing.js");
-  importTest("addGame", "./API/GameAPI_Unit_Testing.js");
+  // importTest("addGame", "./API/GameAPI_Unit_Testing.js");
 
   after(async function () {
       console.log("Clear the fake username from the DB");
