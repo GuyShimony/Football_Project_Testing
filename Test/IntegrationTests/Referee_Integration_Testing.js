c44795ee58e4d376bdd2a59b5e0cd1fd36dc5564
edenyav <edenyav@post.bgu.ac.il>
2021-06-09 11:17:28 +0300

    Changed file name

--- Test/IntegrationTests/Referee_Integration_Testing.js
+++ Test/IntegrationTests/Referee_Integration_Testing.js
@@ -52,54 +52,6 @@ describe('Intergration of Game Domain and Referees Domain', () => {
 
 
 describe('Intergration of Game Domain and Referees Domain', () => {
-    let before_game;
-    before(async () => {
-        before_game = await users_utils.checkIfFreeHeadReferees( "2021-01-03", "19:00:00")
-    })
-    context('Add a game on a day with an existing game and check the effects on the referee', function() {
-        it('should return 0 free referees for that date', async  function() {
-          const test_data = 
-          {
-            game_date: "2021-01-03",
-            game_time: "19:00:00",
-            home_team: "Midtjylland",
-            home_team_id: 939,
-            away_team: "Horsens",
-            away_team_id: 211,
-            stadium: "MCH Arena",
-            headreferee: {user_id: 4, name:"William Collum"},
-            linereferee1: {user_id: 3, name:"Daiyrbek Abdyldayev"},
-            linereferee2: {user_id: 5, name:"Zainiddin Alimov"},
-            boxreferee1: {user_id:6, name:"Bobby Madden"},
-            boxreferee2: {user_id: 2, name:"Denis Shalayev"}
-          } 
-          await games_utils.addFutureGame(
-            test_data.game_date,
-            test_data.game_time,
-            test_data.home_team,
-            test_data.home_team_id,
-            test_data.away_team,
-            test_data.away_team_id,
-            test_data.stadium,
-            test_data.headreferee,
-            test_data.linereferee1,
-            test_data.linereferee2,
-            test_data.boxreferee1,
-            test_data.boxreferee2
-            )
-            // Check if the referees number had changed
-          const after_add = await users_utils.checkIfFreeHeadReferees( "2021-01-03", "19:00:00")
-          common.expect(before_game.length).to.not.equal(after_add.length)
-        })
-      })
-
-      after(async () => {
-        console.log("Deleting the game that was added in the test")  
-        await common.DButils.execQuery(`DELETE FROM Games Where
-       GameDateTime = '2021-01-03 19:00:00' AND HomeTeamID = 939 AND AwayTeamID = 211`)})
-})
-
-describe('Intergration of Game Domain and Referees Domain', () => {
 
     context('Add a new referee to the referees table and then add him to a new game', function() {
         it(`should return 1 free all scheduled games and the referee
--- Test/Unit_Test.Test.js
+++ Test/Unit_Test.Test.js
@@ -16,10 +16,10 @@ describe("Starting to run tests", function () {
 
   const common = require("./common.js");
 
-  // before(async function () {
-  //   console.log( "Creating a fake user name for the api tests in the DB");
-  //   await common.createFakeUser();
-  // });
+  before(async function () {
+    console.log( "Creating a fake user name for the api tests in the DB");
+    await common.createFakeUser();
+  });
   //importTest("Game scheduling", "./Game_Schedule/games_utils_Unit_Testing.js");
   //importTest("Login", "./IntegrationTests/LoginAPI_Integration_Testing.js");
   // before(async function () {
@@ -28,17 +28,17 @@ describe("Starting to run tests", function () {
   // });
   // importTest("Game scheduling", "./UnitTests/Games_Unit_Testing.js");
   //importTest("Referee scheduling", "./UnitTests/Referees_Unit_Testing.js");
+  importTest("Game API", "./IntegrationTests/GameAPI_Integration_Testing.js");
 
-  //importTest("Login", "./API/LoginAPI_Unit_Testing.js");
   //importTest("Team Tests", "./UnitTests/Teams_Unit_Testing.js");
   //importTest("Regression Tests", "./RegressionTests/Regression_Testing.js");
   //importTest("Login Tests", "./UnitTests/Login_Unit_Testing.js");
   // importTest("Games Integration Tests", "./IntegrationTests/Game_Integration_Testing.js");
-  importTest("Referees Integration Tests", "./IntegrationTests/Referee_Integration_Testing.js");
+  // importTest("Referees Integration Tests", "./IntegrationTests/Referee_Integration_Testing.js");
 
-  // after(async function () {
-  //     console.log("Clear the fake username from the DB");
-  //     await common.deleteFakeUser();
-  // });
+  after(async function () {
+      console.log("Clear the fake username from the DB");
+      await common.deleteFakeUser();
+  });
 });
 
